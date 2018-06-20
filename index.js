aae1c2c948e048e97224669b008c1ebb54d36e89
Sean Oughton <seanoughton@gmail.com>
2018-06-20 19:29:31 +0000

    Done.

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ function congratulateCustomer(){
 
 function attemptTwoFavoriteCustomers(){
     let favoriteCustomer = 'bar'
-    let favoriteCustomer = 'not bar'
+    let favoriteCustomer = 'not bar';
 }
